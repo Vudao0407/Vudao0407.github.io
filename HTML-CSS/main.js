55e94b9cc4007114b96a2653a974214784f43972
Vudao0407 <vuhuudaoinc@gmail.com>
2021-07-14 19:11:44 +0700

    new-update

--- HTML-CSS/main.js
+++ HTML-CSS/main.js
@@ -46,66 +46,11 @@ var btn3 = document.getElementById("btn3");
 var btn4 = document.getElementById("btn4");
 var btn5 = document.getElementById("btn5");
 var contact = document.getElementById("contact");
+var off = document.getElementById("contact");
 
-function openBreakfast() {
-  breakfast.style.transform = "translateX(0)";
-  lunch.style.transform = "translateX(100%)";
-  snacks.style.transform = "translateX(100%)";
-  dinner.style.transform = "translateX(100%)";
-  btn2.style.color = "#c59d5f";
-  btn3.style.color = "#000";
-  btn4.style.color = "#000";
-  btn5.style.color = "#000";
-  breakfast.style.transitionDelay = "0.3s";
-  lunch.style.transitionDelay = "0s";
-  snacks.style.transitionDelay = "0s";
-  dinner.style.transitionDelay = "0s";
-}
-
-function openLunch() {
-  debugger;
-  breakfast.style.transform = "translateX(100%)";
-  lunch.style.transform = "translateX(0)";
-  snacks.style.transform = "translateX(100%)";
-  dinner.style.transform = "translateX(100%)";
-  btn3.style.color = "#c59d5f";
-  btn2.style.color = "#000";
-  btn4.style.color = "#000";
-  btn5.style.color = "#000";
-  breakfast.style.transitionDelay = "0s";
-  lunch.style.transitionDelay = "0.3s";
-  snacks.style.transitionDelay = "0s";
-  dinner.style.transitionDelay = "0s";
-}
-
-function openSnacks() {
-  breakfast.style.transform = "translateX(100%)";
-  lunch.style.transform = "translateX(100%)";
-  snacks.style.transform = "translateX(0)";
-  dinner.style.transform = "translateX(100%)";
-  btn4.style.color = "#c59d5f";
-  btn3.style.color = "#000";
-  btn2.style.color = "#000";
-  btn5.style.color = "#000";
-  breakfast.style.transitionDelay = "0s";
-  lunch.style.transitionDelay = "0s";
-  snacks.style.transitionDelay = "0.3s";
-  dinner.style.transitionDelay = "0s";
-}
-function openDinner() {
-  breakfast.style.transform = "translateX(100%)";
-  lunch.style.transform = "translateX(100%)";
-  snacks.style.transform = "translateX(100%)";
-  dinner.style.transform = "translateX(0)";
-  btn5.style.color = "#c59d5f";
-  btn3.style.color = "#000";
-  btn4.style.color = "#000";
-  btn2.style.color = "#000";
-  breakfast.style.transitionDelay = "0s";
-  lunch.style.transitionDelay = "0s";
-  snacks.style.transitionDelay = "0s";
-  dinner.style.transitionDelay = "0.3s";
-}
 function showContact() {
   contact.style.display = "block";
+}
+function modalOff() {
+  contact.style.display = "none";
 }
